df905611b3481831a2187cc3aafaeb3821d315b0
Wojtek <wojtekmikula1@gmail.com>
2025-03-27 21:33:09 +0100

    created Swiftcode Exception Handler

---
+++ src/main/java/org/example/swift_code/SwiftCodeExceptionHandler.java
@@ -0,0 +1,15 @@
+package org.example.swift_code;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestControllerAdvice
+public class SwiftCodeExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<String> handleResponseStatusException(ResponseStatusException ex) {
+        return new ResponseEntity<>(ex.getReason(), ex.getStatusCode());
+    }
+}
